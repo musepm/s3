8e3262af786528f275b9078323850c3a3464fbf8
Jason Livesay <ithkuil@gmail.com>
2015-07-21 12:56:14 +0000

    enable

--- src/signup/index.js
+++ src/signup/index.js
@@ -16,19 +16,23 @@ async function go() {
 
 ---------------------------------------------------------------
 
-Enter ypur app/acctid and AWS access id/secret credentials below.
+Enter your app/acctid and AWS access id/secret credentials below.
 
 `
   console.log(su);
 
   prompts.push('accountid', 'appid', 'id', 'secret');
   let info = await promptGet(prompts);
-  credentials.newCredentials(slack);
+  let main = { accountid: info.accountid, 
+               appid: info.appid };
+  credentials.newCredentials(main);
+  let aws = { id: info.id, secret: info.secret };
+  credentials.newCredentials(aws);  
 }
 
 try {
   go().then(r => {
-    console.log('Done');
+    console.log('Done.');
   });
 } catch(e) {
   console.error(e);
